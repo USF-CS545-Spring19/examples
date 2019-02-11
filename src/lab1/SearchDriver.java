6c8e754a549d049e8c273c18467326f0c5069c48
Olga Karpenko <okarpenko@usfca.edu>
2019-02-10 18:00:48 -0800

    Lab on searching.

---
+++ src/lab1/SearchDriver.java
@@ -0,0 +1,32 @@
+package lab1;
+
+/** Driver class for Linear and Binary Search */
+public class SearchDriver {
+    public static void main(String[] args) {
+        // Create an array of Comparable-s - try it with Strings first, then
+        // references to objects of some custom class like Student
+        Comparable [] arr = {"dog", "show", "cat", "elephant", "cot", "tin", "car", "app", "tick", "dim" };
+
+        // Run linear search and print the result
+        // FILL IN CODE
+
+        // Run recursive linear search and print the result
+        // FILL IN CODE
+
+        // Compute the running time fot non-recursive and recursive linear search
+        // Show TA on the paper
+
+
+        Comparable [] arrSorted = {"app", "bin", "cat", "cot", "crate", "dim", "dome", "elf", "nose", "sam", "seam"};
+        // Run binary search and print the result
+        // FILL IN CODE
+
+
+        // Run recursive binary search and print the result
+        // FILL IN CODE
+
+
+        // Compute the running time fot non-recursive and recursive binary search
+        // Write on paper and show the TA
+    }
+}
