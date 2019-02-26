297047346bf2f406198888b0c7ef8a64fcfbd7ad
Olga Karpenko <okarpenko@usfca.edu>
2019-02-25 23:27:02 -0800

    Added radix sort.

--- src/sorting/SortingDriver.java
+++ src/sorting/SortingDriver.java
@@ -5,14 +5,16 @@ import java.util.Scanner;
 /** Driver class for SortingAlgorithms */
 public class SortingDriver {
     public static void main(String[] args) {
-        //int[] arr = {15, 5, 1, 9, 2, 10, 12, 4};
+
         int[] arr = {17, 10, 15, 13, 4, 12, 7, 9, 16, 8, 5, 14, 3 };
-        System.out.println("Input array: " + Arrays.toString(arr));
         Scanner sc = new Scanner(System.in);
         System.out.println("Which sorting algorithm you'd like to sort it with? ");
-        System.out.println("Type one of the following: selection, bubble, insertion, shell, merge, quickSort");
+        System.out.println("Type one of the following: selection, bubble, insertion, shell, merge, quickSort, radixSort");
 
         String input = sc.nextLine();
+        if (!input.equals("radixSort"))
+            System.out.println("Input array: " + Arrays.toString(arr));
+
         if (input.equals("selection"))
           SortingAlgorithms.selectionSort(arr);
         else if (input.equals("bubble"))
@@ -23,11 +25,21 @@ public class SortingDriver {
             SortingAlgorithms.mergeSort(arr);
         else if (input.equals("quickSort"))
             SortingAlgorithms.quickSort(arr);
+        else if (input.equals("radixSort")) {
+            Elem[] records = {new Elem(711, "a"), new Elem(234, "c"), new Elem(603, "b"), new Elem(241, "d") };
+            SortingAlgorithms.radixSort(records);
+            for (int k =  0 ; k < records.length; k++) {
+                System.out.print("(" + records[k].key() +  " " + records[k].data() + "); ");
+            }
+            System.out.println();
+            return;
+        }
         else {
             System.out.println("Not available.");
             return;
         }
         System.out.print("After sorting with " + input  + ": ");
         System.out.println(Arrays.toString(arr));
+
     }
 }
