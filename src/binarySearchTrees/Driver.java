decb160b1c46c1e35868d7bf3130e1a49710c22e
Olga Karpenko <okarpenko@usfca.edu>
2019-03-22 00:32:19 -0700

    Added binary search tree code.

---
+++ src/binarySearchTrees/Driver.java
@@ -0,0 +1,15 @@
+package binarySearchTrees;
+
+/** Driver class for the BinarySearchTree class */
+public class Driver {
+    public static void main(String[] args) {
+        BinarySearchTree tree = new BinarySearchTree();
+        tree.insert(14);
+        tree.insert(3);
+        tree.insert(25);
+        tree.insert(6);
+        tree.insert(1);
+        tree.printPreorder();
+
+    }
+}
